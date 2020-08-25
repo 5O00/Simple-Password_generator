f6e9b6e38d74c668e70edfc1afd5993388d4026e
5000 <63864045+5O00@users.noreply.github.com>
2020-08-25 15:25:00 +0200

    Update code_generator.py

--- code_generator.py
+++ code_generator.py
@@ -1,11 +1,11 @@
 import random, string
 
-characters = string.printable
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 password = ""
 password_size = int(input("Enter the size of you're password : "))
 
 for i in range(password_size):
-    num = random.randint(0, 99)
+    num = random.randint(0, len(characters)-1)
     password = password + characters[num]
 
 print(password)
